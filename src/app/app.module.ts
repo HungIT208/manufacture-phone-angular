b781d482ecccd406e274b96824011631d3a8bdfd
hungict208 <hungict208@gmail.com>
2021-12-03 14:46:59 +0700

    commit source

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ManufacturerComponent } from './manufacturer/manufacturer.component';
+import { PhoneComponent } from './phone/phone.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    ManufacturerComponent,
+    PhoneComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+          {
+            path: '',
+            component: WelcomeComponent
+          },
+          {
+            path: 'manufacturer',
+            component: ManufacturerComponent
+          },
+          {
+            path: 'phone',
+            component: PhoneComponent
+          }
+       ])
   ],
   providers: [],
   bootstrap: [AppComponent]
